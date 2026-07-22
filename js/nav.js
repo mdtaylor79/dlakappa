a9f541584b002f77f9d38185b2593aba0c90b0df
Marcus D. Taylor <mdtaylor@martaucydesigns.biz>
2026-07-22 04:03:04 -0500

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -127,4 +127,10 @@
       });
     });
   });
+
+  // Tidio chat widget
+  const tidio = document.createElement('script');
+  tidio.src = '//code.tidio.co/roka7o1avkjubn9zot6ve3xaq4mdbldr.js';
+  tidio.async = true;
+  document.body.appendChild(tidio);
 })();
